b35af6dd61e51e85516b2b90341f1a78eba8389e
inigo <ladrongue5@gmail.com>
2022-11-14 08:51:36 +0100

    crear cuenta

---
+++ www/php/CrearCuenta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registro</title>
+    <link rel="stylesheet" href="../css/CrearCuenta.css">
+</head>
+<body>
+    <!--CREAR USUARIO-->
+    <div class="formulario">
+            <h2>Crea tu Cuenta</h2>
+            <form action="#">
+                <input type="text" id="" name ="" placeholder="Usuario" required>
+
+                <input type="password" id="" name ="" placeholder="Contraseña" required>
+
+                <input type="password" id="" name ="" placeholder="Confirmar Contraseña" required>
+
+                <input type="email" id="" name ="" placeholder="Correo Electronico" required>
+
+                <input type="text" id="" name ="" placeholder="Teléfono" required>
+
+                <input type="submit" value="Registrarse">
+            </form>
+        <div class="borrar-password">
+            <a href="borrar_password.php">¿Has olvidado la contraseña?</a>
+        </div>
+    </div>
+</body>
+</html>
