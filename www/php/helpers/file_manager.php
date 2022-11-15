1c12d9492b3a45f3d7b078a6dad48bbaf50223aa
Ameer Hamza <ameer.hamza@ikasle.egibide.org>
2022-11-15 10:24:32 +0100

    parte servidor para subir las imagenes

--- www/php/helpers/file_manager.php
+++ www/php/helpers/file_manager.php
@@ -1,2 +1,64 @@
 <?php
 
+
+function subirFoto($foto) // hay pasar este parametro como $_FILES['nombre_del_campo_de_imagen]
+{
+    $mensaje = "";
+    $directorio = "../../imagenes/";
+    $archivo = $directorio . basename($foto['name']);
+    $estadoSubir = 0;
+    $tipoImage = strtolower(pathinfo($archivo,PATHINFO_EXTENSION));
+
+    $check = getimagesize($foto['tmp_name']);
+
+    if($check !== false)
+    {
+        $estadoSubir = 1;
+    }
+    else{
+        $estado = 0;
+    }
+
+    // comprobar si ya existe un fichero con mismo nombre
+    if(file_exists($archivo)){
+        $mensaje = "El archivo ya existe en el directorio.";
+        $estado = 0;
+    }
+
+    // comprobar si archivo es mayor que 5MB
+    if($foto['size'] > 500000){
+        $mensaje = "El tamaño del archivo es demasiado largo.";
+        $estadoSubir = 0;
+    }
+
+    // comprobar las extensiones de la imagen
+    if($tipoImage != "jpg" && $tipoImage != "png" && $tipoImage != "jpeg")
+    {
+        $mensaje = "Solo se puede subir los archivos con extension jpg, png, jpeg.";
+        $estadoSubir = 0;
+    }
+
+
+    if($estadoSubir !== 0)
+    {
+        if(move_uploaded_file($foto['tmp_name'],$archivo))
+        {
+            $mensaje = "imagen subido con exito.";
+        }
+        else{
+            $mensaje = "Error, subiendo la imagen.",
+        }
+    }
+
+    return ["estado" => $estadoSubir,"mensaje" => $mensaje];
+
+}
+
+function borrarFoto($rutaFichero){
+    $estado = false;
+    if(file_exists($rutaFichero)){
+        unlink($rutaFichero);
+        return true;
+    }
+    return false;
+}
--- www/php/testameer.php
+++ www/php/testameer.php
@@ -6,10 +6,76 @@ require 'models/pregunta.php';
 $datos = ["titulo" => "creando una pregunta","descripcion" => "intentando resolver la pregunta"];
 
 //var_dump(preguntaConRespuestas(connect(),6)->fetchAll());
-foreach(buscarPreguntasPorTitulo(connect(),"pregunta") as $row)
-{
-    echo "Titulo: " . $row['titulo'];
-    echo "<br>";
-    echo "Descripcion: " . $row['descripcion'];
 
-}
+ if(isset($_POST['submit']))
+ {
+    $target_dir = "../imagenes/";
+   
+    $target_file = $target_dir . basename($_FILES['perfil']['name']);
+    echo $target_file . "<br>";
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    $check = getimagesize($_FILES['perfil']['tmp_name']);
+    if($check !== false){
+        echo "File is an valid image -" . $check['mime'] . ".";
+        $uploadOk = 1;
+    }
+    else
+    {
+        echo "File is not a valid image";
+        $uploadOk = 0;
+    }
+
+        if(file_exists($target_file)){
+            echo "file already exists in the directory";
+            $uploadOk = 0;
+        }
+
+        if($_FILES['perfil']['size'] > 5000000)
+        {
+            echo "your file is too large.";
+            $uploadOk = 0;
+        }
+
+        if($imageFileType !== 'jpg' && $imageFileType != "png" && $imageFileType != "jpeg")
+        {
+            echo "Sorry, only JPG;PNG;JPEG extensions are allowed";
+            $uploadOk = 0;
+        }
+
+        if($uploadOk == 0){
+            echo "Sorry your image was not uploaded";
+        }
+        else
+        {
+            try{
+                move_uploaded_file($_FILES['perfil']['tmp_name'],$target_file);
+                echo "image uploaded";
+            }
+            catch(Exception $e){
+                echo $e->getMessage();
+            }
+            
+        }
+
+ }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File upload</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="perfil">
+        <input type="submit" name="submit" value="Subir Foto">
+    </form>
+</body>
+</html>
