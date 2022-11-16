03e580edeb30df167875da3d331456f2a98ddc96
Ameer Hamza <ameer.hamza@ikasle.egibide.org>
2022-11-16 13:53:10 +0100

    validar y registrar el usuario

--- www/php/auth/registrar.php
+++ www/php/auth/registrar.php
@@ -3,10 +3,59 @@
 session_start();
 
 require "../models/usuario.php";
+require "../helpers/file_manager.php";
+require "../basedatos.php";
 
-if(!isset($_POST['nombre']) || !isset($_POST['apellido']) || !isset($_POST['alias']) || !isset($_POST['email']) || !isset($_POST['password']) || !isset($_POST['confirm_password']))
+function vuelveAtras(){
+    header("location: /php/crear_cuenta.php");
+}
+
+// rellanar los datos en una sesion si por algun error tenemos que ir atras podemos rellanar el formulario
+function rellenarOldInputs(){
+    foreach($_POST as $key => $value)
+    {
+        $_SESSION['old'][$key] = $value;
+    }
+}
+
+rellenarOldInputs();
+
+// registrando los errores en una sesion
+
+if(!isset($_POST['nombre']) || $_POST['nombre'] == "")
 {
-    header("location: /php/iniciosesion.php");
+    $_SESSION['errors']['nombre'] = "El campo Nombre es obligatorio";
+    vuelveAtras();
+}
+
+if(!isset($_POST['apellido']) || $_POST['apellido'] == "")
+{
+    $_SESSION['errors']['apellido'] = "El campo Apellido es obligatorio";
+    vuelveAtras();
+}
+
+if(!isset($_POST['email']) || $_POST['email'] == "")
+{
+    $_SESSION['errors']['email'] = "El campo Email es obligatorio";
+    vuelveAtras();
+}
+
+if(!isset($_POST['password']) || $_POST['password'] == "")
+{
+    $_SESSION['errors']['password'] = "El campo Password es obligatorio";
+    vuelveAtras();
+}
+
+if(!isset($_POST['confirm_password']) || $_POST['confirm_password'] == "")
+{
+    $_SESSION['errors']['password'] = "El campo Password es obligatorio";
+    vuelveAtras();
+}
+
+if( $_POST['confirm_password'] != $_POST['password'])
+{
+    $_SESSION['errors']['cpassword'] = "El campo Confirmar Contraseña no coincide con el campo Contraseña";
+    vuelveAtras();
 }
 
 
@@ -14,12 +63,27 @@ $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['email'];
 $alias = $_POST['alias'];
-$password = $_POST['password'];
-$imagen = $_POST['imagen'];
+$_POST['password'] = password_hash($_POST['password'],PASSWORD_BCRYPT);
+
+$imagen = "imagenes/profile.png"; // una imagen por defecto en caso de que el usuario no suba la foto
+if(isset($_POST['imagen']))
+{
+    $result = subirFoto($_POST['perfil'],"../../imagenes/");
+    if($result['estado'] == 1)
+    {
+        $_POST['imagen'] = $result['ruta'];
+    }
+}
+else{
+    $_POST['imagen'] = "imagenes/profile.png";
+}
+
 
 $dbc = connect();
 
 $usuarioInsertado =  insertarUsuario($dbc,$_POST);
+
+// TODO : comprobar si se ha creado el usuario y mandar a la pagina inicial o a la anterior con algun mensaje
 
 var_dump($usuarioInsertado);
 exit;
