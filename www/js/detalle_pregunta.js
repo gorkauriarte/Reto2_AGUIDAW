1c940810a7c1db2c2f4b859cb95ea64c195904f7
gorkauriarte <gorka.uriarte@ikasle.egibide.org>
2022-11-25 08:30:51 +0100

    js cookie

--- www/js/detalle_pregunta.js
+++ www/js/detalle_pregunta.js
@@ -3,32 +3,49 @@ window.addEventListener("load", contarVecesVisitadas);
 function contarVecesVisitadas() {
     misCookies = document.cookie;
     listaCookies = misCookies.split(";")
+
     alert(listaCookies);
-    var existe = "false";
+
+    var existe = false;
 
     for (i in listaCookies) {
         busca = listaCookies[i].search("vecesVisitado");
         if (busca > -1) {
-            existe = "true";
+            existe = true;
             break;
         }
-        
     }
   
     alert(existe);
     
-    if (existe = "false") 
+    if (existe = false) 
     {
         document.cookie = "vecesVisitado=1";
-        alert("no");
+       
     }
     else
     {
-        numero = listaCookies[i];
-        alert("si");
-        document.cookie = numero + 1;
+        document.cookie = "vecesVisitado";
+        
+        alert(numero);
+    }
+
+
+    var visits = GetCookie("vecesVisitado");
+    /*
+
+    if (!visits) { visits = 1;
+
+    document.write("By the way, this is your first time here.");
 
     }
 
-    
+    else {
+
+    visits = parseInt(visits) + 1;
+
+    document.write("I note, you have been here " + visits + " times.");}
+
+    setCookie("counter", visits,expdate);
+    */
 }
